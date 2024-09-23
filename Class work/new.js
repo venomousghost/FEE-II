29f31f09d46de9d2dc078518b1cf8e74d2a8039c
Manan <2005manangupta@gmail.com>
2024-09-23 10:16:51 +0530

    assignment updated

--- Class work/new.js
+++ Class work/new.js
@@ -1,6 +1,14 @@
-let a=10;
-console.log(a)
-a=30;
-console.log(typeof(a))
-let b=3.14;
-console.log(typeof(b))
+console.log(1+2);
+console.log(1+"2");
+console.log(10-"2");
+console.log(typeof(10-"2"));
+console.log(10+"A");
+console.log(typeof(10+"A"));
+console.log(10-"A");
+console.log();
+console.log(1==1);
+
+console.log(1=="1");
+console.log(1=="A");
+console.log(0==[]);
+console.log(0==" ")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
